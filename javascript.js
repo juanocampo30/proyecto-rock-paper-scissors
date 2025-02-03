06d485e0110f78768bbd7dbb4e2138b81a2779a2
Juan Ocampo <164235630+juanocampo30@users.noreply.github.com.>
2025-02-02 19:27:09 -0600

    Todo terminado papu

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
 let min = 1;
 let max = 4;
-let humanScore = 0;
-let computerScore = 0;
+let contadorGeneral = 0;
+let marcadorH = 0;
+let marcadorC = 0;
+let marcador = document.querySelector('.marcador');
+let juegosJugados = document.createElement('span');
+juegosJugados.textContent = `Juegos jugados: ${contadorGeneral}`;
+juegosJugados.style.color = 'black';
+marcador.appendChild(juegosJugados);
+let spanHumano = document.createElement('span');
+spanHumano.textContent = `Tu marcador: ${marcadorH}`;
+spanHumano.style.color = 'black';
+marcador.appendChild(spanHumano);
+let spanComputadora = document.createElement('span');
+spanComputadora.textContent = `Computadora: ${marcadorC}`;
+spanComputadora.style.color = 'black';
+marcador.appendChild(spanComputadora);
+
 
 function getComputerChoice() {
     let resultado = Math.floor(Math.random() * (max - min) + min);
@@ -14,53 +29,90 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let respuestahumana = prompt("Escoje entre piedra, papel o tijeras").toLowerCase();
-    return respuestahumana;
-}
+function playRound(humanSelection, computerSelection) {
 
-function playGame() {
-
-
-    for (let i = 0; i < 5; i++) {
-
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();
-
-        function playRound(humanSelection, computerSelection) {
-
-            if (humanSelection === computerSelection) {
-                alert("Es un empate");
-                alert("Este es ahora el marcador de la computadora: " + computerScore);
-                alert("Este es ahora tu marcador: " + humanScore);
-            } else if (humanSelection === "tijeras" && computerSelection === "papel" || humanSelection === "papel" && computerSelection === "piedra" || humanSelection === "piedra" && computerSelection === "tijeras") {
-                humanScore++;
-                alert("Ganaste");
-                alert("Este es ahora el marcador de la computadora: " + computerScore);
-                alert("Este es ahora tu marcador: " + humanScore);
-            } else {
-                computerScore++;
-                alert("Gano la computadora");
-                alert("Este es ahora el marcador de la computadora: " + computerScore);
-                alert("Este es ahora tu marcador: " + humanScore);
-            }
-
-        }
-
-        playRound(computerSelection, humanSelection);
-        console.log("Tu marcador: " + humanScore);
-        console.log("Computadora: " + computerScore);
-
-    }
-
-    if (humanScore > computerScore) {
-        alert("Ganaste eres genial")
-    } else if (humanScore === computerScore) {
-        alert('Es un empate')
+    if (humanSelection === computerSelection) {
+        alert("Es un empate");
+        contadorGeneral++;
+    } else if (humanSelection === "tijeras" && computerSelection === "papel" || humanSelection === "papel" && computerSelection === "piedra" || humanSelection === "piedra" && computerSelection === "tijeras") {
+        alert("Ganaste");
+        marcadorH++;
+        contadorGeneral++;
+        spanHumano.textContent = `Tu marcador: ${marcadorH}`;
+        spanComputadora.textContent = `Computadora: ${marcadorC}`;
     } else {
-        alert('La computadora gano :(')
+        alert("Gano la computadora");
+        marcadorC++;
+        contadorGeneral++;
+        spanHumano.textContent = `Tu marcador: ${marcadorH}`;
+        spanComputadora.textContent = `Computadora: ${marcadorC}`;
+    }
+
+    juegosJugados.textContent = `Juegos jugados: ${contadorGeneral}`;
+
+    if (contadorGeneral === 5) {
+        spanHumano.remove();
+        spanComputadora.remove();
+        juegosJugados.remove();
+        document.removeEventListener('click', accionar);
+        if (marcadorH > marcadorC) {
+            let resultado = document.createElement('span');
+            resultado.textContent = `Ganaste por ${marcadorH} a ${marcadorC} eres hermoso \u2764\uFE0F`;
+            resultado.style.fontSize = '24px';
+            marcador.appendChild(resultado);
+            let jugarOtro = document.createElement('button');
+            jugarOtro.textContent = 'Volver a jugar';
+            jugarOtro.addEventListener('click', e => {
+                location.reload();
+            })
+            marcador.appendChild(jugarOtro);
+        } else if (marcadorC > marcadorH) {
+            let resultado = document.createElement('span');
+            resultado.textContent = `Perdiste por ${marcadorC} a ${marcadorH} pero igual eres hermoso \u2764\uFE0F`;
+            resultado.style.fontSize = '24px';
+            marcador.appendChild(resultado);
+            let jugarOtro = document.createElement('button');
+            jugarOtro.textContent = 'Volver a jugar';
+            jugarOtro.addEventListener('click', e => {
+                location.reload();
+            })
+            marcador.appendChild(jugarOtro);
+        } else if (marcadorC === marcadorH) {
+            let resultado = document.createElement('span');
+            resultado.textContent = 'Buuu empate que aburrido, vuelve a intentar :)';
+            resultado.style.fontSize = '24px';
+            marcador.appendChild(resultado);
+            let jugarOtro = document.createElement('button');
+            jugarOtro.textContent = 'Volver a jugar';
+            jugarOtro.addEventListener('click', e => {
+                location.reload();
+            })
+            marcador.appendChild(jugarOtro);
+        }
+    }
+
+}
+
+function accionar(e) {
+    if (e.target.matches('.piedra')) {
+        let computerSelection = getComputerChoice();
+        let humanSelection = 'piedra';
+        playRound(humanSelection, computerSelection);
+    } else if (e.target.matches('.papel')) {
+        let computerSelection = getComputerChoice();
+        let humanSelection = 'papel';
+        playRound(humanSelection, computerSelection);
+    } else if (e.target.matches('.tijeras')) {
+        let computerSelection = getComputerChoice();
+        let humanSelection = 'tijeras';
+        playRound(humanSelection, computerSelection);
     }
 }
 
-playGame();
+document.addEventListener('click', accionar)
+
+
+
+
+
 
